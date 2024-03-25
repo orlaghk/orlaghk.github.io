c7d428145050c36865d50ecc7162526759468d15
Orlagh Keane <S2084384@ed.ac.uk>
2024-03-25 16:19:41 +0000

    fixed duplicate rows

--- add_og_dates.py
+++ add_og_dates.py
@@ -2,14 +2,11 @@ import pandas as pd
 
 # Read the peoples_beeswarm DataFrame
 df = pd.read_csv("odnb_beeswarm.csv")
-df_creator = pd.read_csv("odnb_beeswarm copy.csv")
 
-# Merge the DataFrames based on the 'titles' column
-merged_df = pd.merge(df, df_creator[['title', 'cleaned_creator']], 
-                     on='title', 
-                     how='left')
+# Drop duplicate authors
+df = df.drop_duplicates(subset=['cleaned_creator'], keep='first')
 
-# Rename the 'cleaned_creator' column to avoid duplicate columns
-merged_df.rename(columns={'cleaned_creator': 'matched_cleaned_creator'}, inplace=True)
+# Save the updated DataFrame to a new CSV file
+df.to_csv("odnb_beeswarm_no_duplicates.csv", index=False)
 
-merged_df.to_csv("odnb_beeswarm_with_matched_creator.csv", index=False)
+print("Duplicates dropped and saved to peoples_beeswarm_no_duplicates.csv")
